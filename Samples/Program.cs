47fd6be32d98e784f577ebcf6837669f9cb45b7a
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-07-23 16:33:12 +0330

    Add project files.

---
+++ Samples/Program.cs
@@ -0,0 +1,11 @@
+﻿DDoSomething<object> doSomething = (object obj) => Console.WriteLine(obj);
+
+DDoSomething<string> doSomethinString = doSomething;
+// Using a DDoSomething<object> in place of DDoSomething<string> is legal because of "in"
+
+doSomethinString("text");
+// This is safe because string is used in place of object (derived in place of base)
+
+Console.ReadKey();
+
+public delegate void DDoSomething<in A>(A str);
